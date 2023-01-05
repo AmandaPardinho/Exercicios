1e1cdc9c9fb97f57d46b957549d3e26355f5fb64
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-05 20:53:39 -0300

    Exercise statement

--- Lógica de Programação/Laços de Repetição/Java/Tabuada/src/tabuada/TabuadaAleatoria.java
+++ Lógica de Programação/Laços de Repetição/Java/Tabuada/src/tabuada/TabuadaAleatoria.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class TabuadaAleatoria {
     public static void main(String[] args) {
+        System.out.println("""
+                Faça um programa que receba um número e, usando laços de repetição, calcule e mostre a tabuada desse 
+                número.
+                """);
         Scanner scanner = new Scanner(System.in);
 
         int numero;
