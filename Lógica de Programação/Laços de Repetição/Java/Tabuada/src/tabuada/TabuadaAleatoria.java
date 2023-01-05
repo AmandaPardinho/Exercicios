94eceb000d7b9fd57705a12d932f7003e749df11
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-01-05 20:42:39 -0300

    Create the code for the exercise.

--- Lógica de Programação/Laços de Repetição/Java/Tabuada/src/tabuada/TabuadaAleatoria.java
+++ Lógica de Programação/Laços de Repetição/Java/Tabuada/src/tabuada/TabuadaAleatoria.java
@@ -1,7 +1,19 @@
 package tabuada;
 
+import java.util.Scanner;
+
 public class TabuadaAleatoria {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+
+        System.out.println("Digite um número: ");
+        numero = scanner.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+            int tabuada = numero * i;
+            System.out.println(numero + " x " + i + " = " + tabuada);
+        }
     }
 }
